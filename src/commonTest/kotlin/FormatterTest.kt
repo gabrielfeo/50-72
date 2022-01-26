973554b9fd792f6e1dd9ec6360d092772d6db87d
Gabriel Feo <gabriel@gabrielfeo.com>
2022-01-26 21:04:32 +0000

    Add more tests on subject line

--- src/commonTest/kotlin/FormatterTest.kt
+++ src/commonTest/kotlin/FormatterTest.kt
@@ -2,35 +2,60 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
 
-const val SUBJECT_50 = "01234567890123456789012345678901234567890123456789"
-const val SUBJECT_51 = "012345678901234567890123456789012345678901234567890"
+const val SINGLE_LINE_50 = "01234567890123456789012345678901234567890123456789"
+const val SINGLE_LINE_51 = "012345678901234567890123456789012345678901234567890"
 
-const val SUBJECT_50_BODY_72 = """$SUBJECT_50
+const val SUBJECT_51_BODY_72 = """$SINGLE_LINE_51
 
 012345678901234567890123456789012345678901234567890123456789012345678901
 012345678901234567890123456789012345678901234567890123456789012345678901"""
 
-const val SUBJECT_50_BODY_73 = """$SUBJECT_50
+const val SUBJECT_50_BODY_72 = """$SINGLE_LINE_50
 
 012345678901234567890123456789012345678901234567890123456789012345678901
-0123456789012345678901234567890123456789012345678901234567890123456789012
 012345678901234567890123456789012345678901234567890123456789012345678901"""
+
+const val SUBJECT_50_BODY_73 = """$SINGLE_LINE_50
+
+012345678901234567890123456789012345678901234567890123456789012345678901
+lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum"""
+
+const val SUBJECT_50_BODY_73_FIXED = """$SINGLE_LINE_50
+
+012345678901234567890123456789012345678901234567890123456789012345678901
+lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+lorem lorem ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum
+ipsum ipsum"""
 
 class FormatterTest {
 
     private val formatter = Formatter()
 
     @Test
+    fun failsGivenSingleLineOver50() {
+        val error = assertFails {
+            formatter.format(SINGLE_LINE_51)
+        }
+        assertEquals(HEADING_OVER_50_MESSAGE, error.message)
+    }
+
+    @Test
+    fun doesntFailGivenSingleLineUpTo50() {
+        formatter.format(SINGLE_LINE_50)
+    }
+
+    @Test
     fun failsGivenSubjectLineOver50() {
         val error = assertFails {
-            formatter.format(SUBJECT_51)
+            formatter.format(SUBJECT_51_BODY_72)
         }
         assertEquals(HEADING_OVER_50_MESSAGE, error.message)
     }
 
     @Test
     fun doesntFailGivenSubjectLineUpTo50() {
-        formatter.format(SUBJECT_50)
+        formatter.format(SUBJECT_50_BODY_72)
     }
 
     @Test
