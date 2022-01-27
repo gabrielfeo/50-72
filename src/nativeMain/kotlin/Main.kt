adfc352d277deb20df3487617fade274f1a4cdce
Gabriel Feo <gabriel@gabrielfeo.com>
2022-01-27 00:07:08 +0000

    Implement command-line interaction

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,12 @@
-fun main() {
-    format("")
+import platform.posix.exit
+
+fun main(args: Array<String>) {
+    try {
+        require(args.size == 1) { "Expected args: <commit-message>" }
+        val message = args.first()
+        print(format(message))
+    } catch (error: Throwable) {
+        println(error.message)
+        exit(1)
+    }
 }
