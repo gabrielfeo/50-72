406fdb1a158245fadd5eac82126ce3b05c33803b
Gabriel Feo <gabriel@gabrielfeo.com>
2022-02-03 10:53:54 +0000

    WIP: format through injected function
    not working because function is not self-contained

--- browser-extension/src/jsMain/kotlin/Main.kt
+++ browser-extension/src/jsMain/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import ScriptInjection.InjectionTarget
+import kotlinx.browser.document
+
 external val chrome: dynamic
 class ContextMenu(
     val id: String,
@@ -5,32 +8,60 @@ class ContextMenu(
     val contexts: Array<String>,
 )
 
+class Tab(val id: String)
+
+class ScriptInjection(
+    val target: InjectionTarget,
+    val func: () -> Unit
+) {
+    class InjectionTarget(val tabId: String)
+}
+
+class OnClickData(
+    val menuItemId: String,
+    val editable: Boolean,
+    val selectionText: String?,
+)
+
+private const val MENU_ITEM_ID = "foo"
+
 fun main() {
     chrome.runtime.onInstalled.addListener {
         chrome.contextMenus.create(
             ContextMenu(
-                id = "foo",
-                title = "foo",
+                id = MENU_ITEM_ID,
+                title = MENU_ITEM_ID,
                 contexts = arrayOf(
                     "page",
-                    "selection",
-                    "link",
                     "editable",
-                    "image",
-                    "video",
-                    "audio"
                 ),
             ),
-            { a, b ->
-                console.log("callback: ${a?.toString()} / ${b?.toString()}")
+            {
+                console.log("Menu created!")
             }
         )
-        chrome.contextMenus.onClicked.addListener { a, b ->
-            console.log(a)
-            console.log(b)
+        chrome.contextMenus.onClicked.addListener { data: OnClickData, tab: Tab ->
+            injectFunction(tab) {
+                val descriptionTextArea = document.querySelector("#merge_request_description")
+                descriptionTextArea?.let {
+                    val description = it.textContent
+                    if (description?.isNotBlank() == true) {
+                        it.textContent = format(description)
+                    } else {
+                        TODO("alert")
+                    }
+                }
+            }
         }
         console.log("Hello world!")
     }
 }
 
-const val COLOR = "#3aa757";
+private fun injectFunction(tab: Tab, func: () -> Unit) {
+    val script = ScriptInjection(
+        target = InjectionTarget(tabId = tab.id),
+        func,
+    )
+    console.log("Will inject to tab ${tab.id}")
+    chrome.scripting.executeScript(script)
+}
