56e3bb76cf551f5b0c1d2f603059c44c58811b5d
Gabriel Feo <gabriel@gabrielfeo.com>
2022-02-17 13:22:07 +0000

    Alert when description area isn't found

--- browser-extension/description-formatter/src/jsMain/kotlin/Main.kt
+++ browser-extension/description-formatter/src/jsMain/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import kotlinx.browser.document
+import kotlinx.browser.window
 import org.w3c.dom.Document
 import org.w3c.dom.HTMLTextAreaElement
+
+const val SUPPORT_ADDRESS = "gabriel@gabrielfeo.com"
 
 fun main() {
     val descriptionArea = findCommitMessageBodyTextArea()
@@ -8,7 +11,8 @@ fun main() {
     if (description?.isNotBlank() == true) {
         descriptionArea.value = formatBody(description)
     } else {
-        TODO("alert failed to format description. area=$descriptionArea / content=$description")
+        alertFailedToFindDescriptionArea()
+        logFailedToFindDescriptionArea(descriptionArea)
     }
 }
 
@@ -19,3 +23,24 @@ private fun findCommitMessageBodyTextArea(): HTMLTextAreaElement? {
 
 private val Document.gitHubDescriptionArea get() = querySelector("#pull_request_body")
 private val Document.gitLabDescriptionArea get() = querySelector("#merge_request_description")
+
+private fun alertFailedToFindDescriptionArea() {
+    window.alert("""
+        Couldn't find an PR/MR description area in this page :(
+        
+        Please report this to $SUPPORT_ADDRESS with:
+          • the current page URL (please remove any sensitive data)
+          • a screenshot of the page's Console (only the log entry starting with [50-72])
+    """.trimIndent())
+}
+
+private fun logFailedToFindDescriptionArea(descriptionArea: HTMLTextAreaElement?) {
+    console.log(
+        """
+            [50-72] Failed to format description.
+              descriptionArea=$descriptionArea
+                descriptionArea.textContent=${descriptionArea?.textContent} 
+                descriptionArea.value=${descriptionArea?.value} 
+        """.trimIndent()
+    )
+}
