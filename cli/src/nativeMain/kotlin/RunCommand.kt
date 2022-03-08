714135bf76b53e53d43c2cc9c508dfae4a6142db
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-08 03:11:46 +0000

    Implement --install-hook

--- cli/src/nativeMain/kotlin/RunCommand.kt
+++ cli/src/nativeMain/kotlin/RunCommand.kt
@@ -11,6 +11,15 @@ import okio.buffer
 import okio.use
 
 private const val DEFAULT_GIT_MSG_FILE = ".git/EDIT_COMMITMSG"
+private const val PREPARE_COMMIT_MSG_PATH = ".git/hooks/prepare-commit-msg"
+private const val PREPARE_COMMIT_MSG_SCRIPT = "\n\n50-72 --message-file $1\n"
+
+// TODO Try out subcommands with invokeWithoutSubcommands=true
+// TODO --uninstall-hook: remove line with '50-72' from hook
+// TODO Add internal option for executable path
+// TODO Try replacing messageFile for sh logic in hook
+// TODO chmod hook file
+// TODO Add shebang if new file?
 
 class Main(
     private val format: (message: String, isMarkdown: Boolean) -> String = ::formatFullMessage,
@@ -22,13 +31,27 @@ class Main(
     private val isMarkdown by option("--markdown", help = "Set when message is in Markdown format")
         .flag(default = false)
 
-    private val messageFile by option("--message-file", help = "Path to a commit message file", hidden = true)
+    private val shouldInstallHook by option(
+        "--install-hook",
+        help = "Install git hook to format messages automatically in the current repository",
+    ).flag(default = false)
+
+    private val messageFile by option("--message-file", hidden = true)
 
     override fun run() {
+        if (shouldInstallHook) {
+            installHook()
+            return
+        }
         when (messageFile) {
             null -> formatMessageArgument()
             else -> formatFileAsHook()
         }
+    }
+
+    private fun installHook() {
+        val hookFile = PREPARE_COMMIT_MSG_PATH.toPath()
+        hookFile.appendText(PREPARE_COMMIT_MSG_SCRIPT)
     }
 
     private fun formatMessageArgument() {
@@ -63,6 +86,12 @@ class Main(
     private fun Path.writeText(text: String) {
         FileSystem.SYSTEM.write(this) {
             writeUtf8(text)
+        }
+    }
+
+    private fun Path.appendText(text: String) {
+        FileSystem.SYSTEM.appendingSink(this).buffer().use {
+            it.writeUtf8(text)
         }
     }
 
