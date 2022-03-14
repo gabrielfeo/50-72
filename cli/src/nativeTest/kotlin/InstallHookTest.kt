1b5b6ae2530b64052f64cd5786e1193dbbc3ae7f
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-14 01:07:41 +0000

    Add failing tests on InstallHook

---
+++ cli/src/nativeTest/kotlin/InstallHookTest.kt
@@ -0,0 +1,29 @@
+import kotlin.test.Test
+
+class InstallHookTest {
+
+    @Test
+    fun givenHookExists_WhenInstall_ThenAppendsCommandToHook() {
+        TODO()
+    }
+
+    @Test
+    fun givenNoHook_WhenInstall_ThenCreatesHookWithCommand() {
+        TODO()
+    }
+
+    @Test
+    fun givenNoHook_WhenInstall_ThenCreatesHookWithShebang() {
+        TODO()
+    }
+
+    @Test
+    fun givenHookExistsWithOtherCommands_WhenUninstall_ThenRemovesOurCommand() {
+        TODO()
+    }
+
+    @Test
+    fun givenHookExistsWithOnlyOurCommand_WhenUninstall_ThenDeletesHook() {
+        TODO()
+    }
+}
