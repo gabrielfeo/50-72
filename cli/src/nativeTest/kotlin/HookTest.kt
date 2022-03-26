fbe96599eb43aac0fba032b7d24bb549bb515cd1
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-26 01:18:10 +0000

    Write tests on --uninstall

--- cli/src/nativeTest/kotlin/HookTest.kt
+++ cli/src/nativeTest/kotlin/HookTest.kt
@@ -2,6 +2,7 @@ import okio.Path.Companion.toPath
 import okio.fakefilesystem.FakeFileSystem
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 class HookTest {
 
@@ -18,15 +19,16 @@ class HookTest {
 
     @Test
     fun givenHookExists_WhenInstall_ThenAppendsCommandToHook() {
-        val previousContent = """
+        givenHookExists(
+            """
                 $SHEBANG
                 
                 echo
                 
             """.trimIndent()
-        givenHookExists(previousContent)
+        )
         install()
-        assertEquals(
+        assertHookEquals(
             """
                 $SHEBANG
                 
@@ -34,40 +36,80 @@ class HookTest {
                 
                 $FORMAT_FILE_COMMAND
                 
-            """.trimIndent(),
-            prepareCommitMsg.readText(fileSystem),
+            """.trimIndent()
         )
+    }
+
+    @Test
+    fun givenNoHook_WhenInstall_ThenCreatesHookWithCommandAndShebang() {
+        install()
+        assertHookEquals(
+            """
+                $SHEBANG
+                
+                $FORMAT_FILE_COMMAND
+                
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun givenHookExistsWithOtherCommands_WhenUninstall_ThenRemovesOurCommand() {
+        givenHookExists(
+            """
+                $SHEBANG
+                
+                echo
+                
+                $FORMAT_FILE_COMMAND
+                
+            """.trimIndent()
+        )
+        uninstall()
+        assertHookEquals(
+            expected = """
+                $SHEBANG
+                
+                echo
+                
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun givenHookExistsWithOnlyOurCommand_WhenUninstall_ThenDeletesHook() {
+        givenHookExists(
+            """
+                $SHEBANG
+                
+                $FORMAT_FILE_COMMAND
+                
+            """.trimIndent()
+        )
+        uninstall()
+        assertHookDoesntExist()
+    }
+
+    private fun givenHookExists(content: String) {
+        prepareCommitMsg.writeText(content, mustCreate = true, fileSystem = fileSystem)
     }
 
     private fun install() {
         hook.parse(arrayOf("--install"))
     }
 
-    @Test
-    fun givenNoHook_WhenInstall_ThenCreatesHookWithCommandAndShebang() {
-        install()
+    private fun uninstall() {
+        hook.parse(arrayOf("--uninstall"))
+    }
+
+    private fun assertHookEquals(expected: String) {
         assertEquals(
-            expected = """
-                $SHEBANG
-                
-                $FORMAT_FILE_COMMAND
-                
-            """.trimIndent(),
+            expected,
             actual = prepareCommitMsg.readText(fileSystem),
         )
     }
 
-    @Test
-    fun givenHookExistsWithOtherCommands_WhenUninstall_ThenRemovesOurCommand() {
-        TODO()
-    }
-
-    @Test
-    fun givenHookExistsWithOnlyOurCommand_WhenUninstall_ThenDeletesHook() {
-        TODO()
-    }
-
-    private fun givenHookExists(content: String) {
-        prepareCommitMsg.writeText(content, mustCreate = true, fileSystem = fileSystem)
+    private fun assertHookDoesntExist() {
+        assertFalse(fileSystem.exists(prepareCommitMsg))
     }
 }
