84df7152af49dbb6356c282fcaa0cfdd928b79a7
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-27 16:08:35 +0100

    Improve script

--- .github/steps/update-homebrew-formula.py
+++ .github/steps/update-homebrew-formula.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
+from collections import namedtuple
 import hashlib
+import os
 import requests
 import sys
 
+TAP_REPOSITORY = 'gabrielfeo/homebrew-50-72'
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--version')
-parser.add_argument('--trigger-source', dest='trigger_source')
-parser.add_argument('--username')
-parser.add_argument('--password')
+parser.add_argument('--version', required=True)
+parser.add_argument('--trigger-source', required=True)
+parser.add_argument('--assets', nargs='+', required=True)
 args = parser.parse_args()
 
-linux_zip_location=f"./cli/build/release/50-72-{args.version}-linuxX64.zip"
-macos_zip_location=f"./cli/build/release/50-72-{args.version}-macosX64.zip"
+github_username = os.environ["GITHUB_USERNAME"]
+github_token = os.environ["GITHUB_TOKEN"]
+
+Asset = namedtuple('Asset', ('path', 'sha'))
 
 
 def sha256(path) -> str:
@@ -24,15 +29,18 @@ def sha256(path) -> str:
         return sha.hexdigest()
 
 
-linux_sha = sha256(linux_zip_location)
-macos_sha = sha256(macos_zip_location)
-print('Artifact SHA-256 sums:')
-print(f"{linux_zip_location}: {linux_sha}")
-print(f"{macos_zip_location}: {macos_sha}\n")
+assert len(args.assets) == 2
+for path in args.assets:
+    asset = Asset(path, sha256(path))
+    if 'linux' in path:
+        linux = asset
+    elif 'macos' in path:
+        macos = asset
+    print(asset)
 
 response = requests.post(
-    'https://api.github.com/repos/gabrielfeo/homebrew-50-72/dispatches',
-    auth=(args.username, args.password),
+    f"https://api.github.com/repos/{TAP_REPOSITORY}/dispatches",
+    auth=(github_username, github_token),
     headers={
         'Accept': 'application/vnd.github.v3+json',
     },
@@ -40,13 +48,15 @@ response = requests.post(
         'event_type': 'update-formula',
         'client_payload': {
             'version': args.version,
-            'linux_sha': linux_sha,
-            'macos_sha': macos_sha,
+            'linux_sha': linux.sha,
+            'macos_sha': macos.sha,
             'trigger_source_url': args.trigger_source
         }
     }
 )
 
-if response.status_code not in range(200, 299):
+if response.status_code in range(200, 299):
+    print(f"Trigger successful: {response.status_code}")
+else:
     print(f"Request failed: {response.status_code}\n{response.content}", file=sys.stderr)
     exit(1)
