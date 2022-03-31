ab9a5ee6f727eeaa0b068d413bed4a20e9c8d9b1
Gabriel Feo <gabriel@gabrielfeo.com>
2022-03-31 10:12:26 +0100

    Refactor browser extension plugin
    Centralize logic in build-src. Make browser-extension less generic for
    less complexity.

--- build-src/src/main/kotlin/multiplatform-js-browser-extension.gradle.kts
+++ build-src/src/main/kotlin/multiplatform-js-browser-extension.gradle.kts
@@ -4,7 +4,11 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */        
+ */
+
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.registering
 
 /*
 * A project that bundles together a manifest.json and any number of JS executables
@@ -15,6 +19,23 @@ plugins {
     id("base")
 }
 
+val aggregateSubprojectOutputs by tasks.registering(Copy::class) {
+    destinationDir = file("$buildDir/outputs/extension")
+    val subOutputs = project.subprojects.map {
+        it.tasks.named("jsBrowserDistribution").map { task ->
+            task.outputs
+        }
+    }
+    from(subOutputs, "$projectDir/manifest.json")
+}
+
 tasks.named("assemble") {
-    dependsOn(tasks.withType(aggregate.Aggregate::class))
+    dependsOn(aggregateSubprojectOutputs)
+}
+
+tasks.named("check") {
+    val subCheckTasks = subprojects.map {
+        it.tasks.named("check")
+    }
+    dependsOn(subCheckTasks)
 }
