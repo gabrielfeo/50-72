a1eec23bcf6e48984fc6758fa8a687ba483f23d2
Gabriel Feo <gabriel@gabrielfeo.com>
2022-04-03 02:26:19 +0100

    Re-write message formatter

--- formatter/src/commonMain/kotlin/Format.kt
+++ formatter/src/commonMain/kotlin/Format.kt
@@ -15,10 +15,6 @@ import kotlin.text.RegexOption.MULTILINE
 internal const val HEADING_OVER_50_MESSAGE = "Heading line must not be over 50 columns. Please re-format the heading manually."
 internal const val NO_SUBJECT_BODY_SEPARATOR_MESSAGE = "There must be a blank line between subject and body. Please add the blank line!"
 
-private const val WORD_SPACING = ' '
-private const val WORD_SPACING_SIZE = 1
-private const val SUBJECT_BODY_SEPARATOR = "\n\n"
-
 
 fun formatFullMessage(messageText: String, isMarkdown: Boolean = false): String {
     val message = CommitMessage(messageText)
@@ -27,9 +23,10 @@ fun formatFullMessage(messageText: String, isMarkdown: Boolean = false): String 
         return message.fullText
     }
     require(message.hasSubjectBodySeparator) { NO_SUBJECT_BODY_SEPARATOR_MESSAGE }
-    return buildString {
-        append(message.subject())
-        append(SUBJECT_BODY_SEPARATOR)
+    return buildMessage {
+        appendRaw(message.subject())
+        breakLine()
+        breakLine()
         when {
             isMarkdown -> appendReformattedMarkdownBody(message.body())
             else -> appendReformattedBody(message.body(), stripComments = true)
@@ -38,7 +35,7 @@ fun formatFullMessage(messageText: String, isMarkdown: Boolean = false): String 
 }
 
 fun formatBody(bodyText: String, isMarkdown: Boolean = false): String {
-    return buildString {
+    return buildMessage {
         when {
             isMarkdown -> appendReformattedMarkdownBody(bodyText)
             else -> appendReformattedBody(bodyText, stripComments = true)
@@ -48,16 +45,17 @@ fun formatBody(bodyText: String, isMarkdown: Boolean = false): String {
 
 @Deprecated("Use formatBody with parameter")
 fun formatMarkdownBody(bodyText: String): String {
-    return buildString {
+    return buildMessage {
         appendReformattedMarkdownBody(bodyText)
     }
 }
 
-private fun StringBuilder.appendReformattedMarkdownBody(bodyText: String) {
+private fun MessageBuilder.appendReformattedMarkdownBody(bodyText: String) {
     val body = parseMarkdownBody(bodyText)
     for (section in body.sections) {
         if (this.isNotEmpty()) {
-            append("\n\n")
+            breakLine()
+            breakLine()
         }
         when (section) {
             is Paragraph -> appendReformattedBody(section.content, stripComments = false)
@@ -67,28 +65,24 @@ private fun StringBuilder.appendReformattedMarkdownBody(bodyText: String) {
 }
 
 
-private fun StringBuilder.appendReformattedBody(body: String, stripComments: Boolean) {
-    val bodyContent = when {
-        !stripComments -> body
-        else -> body.lines()
-            .filterNot { it.startsWith('#') }
-            .joinToString("\n")
-    }
-    val words = bodyContent.split(Regex("(?<=\\S)$WORD_SPACING*\\n(?!\\n)|$WORD_SPACING+"))
-    var currentLineColumns = 0
-    fun append(str: String) {
-        this.append(str)
-        if ('\n' in str) currentLineColumns = str.substringAfterLast('\n').count { it != '\n' }
-        else currentLineColumns += str.length
-    }
-    for (word in words) {
+
+private fun MessageBuilder.appendReformattedBody(body: String, stripComments: Boolean) {
+    body.lines().forEach {
         when {
-            currentLineColumns == 0 -> append(word)
-            currentLineColumns + word.length + WORD_SPACING_SIZE <= 72 -> append("$WORD_SPACING$word")
-            else -> append("\n$word")
+            it.isNotEmpty() && it.isNotComment() -> append(it)
+            it.isEmpty() -> {
+                breakLine()
+                breakLine()
+            }
+            stripComments && it.isComment() -> return@forEach
+            it.isEmpty() || it.isComment() -> appendRaw(it)
+            else -> error("Unpredicted case: line '$it'")
         }
     }
 }
+
+private fun String.isComment() = startsWith("#")
+private fun String.isNotComment() = !isComment()
 
 @Suppress("RemoveExplicitTypeArguments")
 private fun parseMarkdownBody(body: String): MarkdownBody {
@@ -142,4 +136,89 @@ private class CommitMessage(fullMessage: String) {
             value class Other(val content: String) : Section
         }
     }
+}
+
+private inline fun buildMessage(block: MessageBuilder.() -> Unit): String =
+    MessageBuilder().apply(block).toString()
+
+/*
+ * Note to self: Current separation of concerns is to keep MessageBuilder with the line-breaking logic onlu. Line and
+ * section splitting is done outside the class. Only consider refactoring this further once all test are passing
+ * including Markdown tests using the new implementation.
+ */
+private class MessageBuilder : CharSequence {
+
+    private var currentLineLength: Int = 0
+
+    private val string = StringBuilder()
+
+    fun breakLine() {
+        string.append('\n')
+        currentLineLength = 0
+    }
+
+    fun appendRaw(value: CharSequence) {
+        string.append(value)
+        val indexOfLastNewline = value.lastIndexOf('\n')
+        if (indexOfLastNewline == -1) {
+            currentLineLength += value.length
+        } else {
+            val lengthAfterNewline = value.length - indexOfLastNewline - 1
+            currentLineLength = lengthAfterNewline
+        }
+    }
+
+    fun append(value: CharSequence) {
+        for (word in value.split(' ')) {
+            val wordIs72OrMore = word.length >= 72
+            if (wordIs72OrMore) {
+                if (currentLineLength == 0) {
+                    string.append("$word\n")
+                    currentLineLength = 0
+                } else {
+                    string.append("\n$word\n")
+                }
+                currentLineLength = 0
+            } else {
+                val mustPrependSpace = currentLineLength != 0
+                val addedLength = when {
+                    mustPrependSpace -> word.length + 1
+                    else -> word.length
+                }
+                val lineWontPass72 = currentLineLength + addedLength <= 72
+                if (lineWontPass72) {
+                    if (mustPrependSpace) {
+                        string.append(' ')
+                    }
+                    string.append(word)
+                    currentLineLength += addedLength
+                } else {
+                    string.append("\n$word")
+                    currentLineLength = addedLength
+                }
+            }
+        }
+    }
+
+    // TODO Remove?
+    private fun append(value: Char) {
+        if (value == '\n') {
+            currentLineLength = 0
+        } else {
+            val lineWouldPass72 = currentLineLength + 1 > 72
+            if (lineWouldPass72) {
+                string.append('\n')
+                currentLineLength = 1
+            }
+        }
+        string.append(value)
+    }
+
+    override val length = string.length
+    override fun get(index: Int) = string[index]
+    override fun subSequence(startIndex: Int, endIndex: Int) = string.subSequence(startIndex, endIndex)
+
+    override fun equals(other: Any?) = other is MessageBuilder && other.string == this.string
+    override fun hashCode() = string.hashCode()
+    override fun toString() = string.toString()
 }
