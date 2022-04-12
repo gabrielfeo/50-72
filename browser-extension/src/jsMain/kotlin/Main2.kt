65ec9a787eb0e5d117a9b617a2c7bf6a3ccfd1ae
Gabriel Feo <gabriel@gabrielfeo.com>
2022-04-12 01:23:38 +0100

    WIP: Format button calls format

--- browser-extension/src/jsMain/kotlin/Main2.kt
+++ browser-extension/src/jsMain/kotlin/Main2.kt
@@ -1,8 +1,14 @@
 import kotlinx.dom.appendElement
 import org.w3c.dom.Element
 
-fun main2(parent: Element) {
+fun main2(
+    parent: Element,
+    format: (String, Boolean) -> String = ::formatBody,
+) {
     parent.appendElement("button") {
         innerHTML = "Format"
     }
+    format("", true)
 }
+
+
--- browser-extension/src/jsTest/kotlin/MainTest.kt
+++ browser-extension/src/jsTest/kotlin/MainTest.kt
@@ -1,21 +1,36 @@
 import kotlinx.browser.document
 import kotlinx.dom.appendElement
 import org.w3c.dom.Element
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import org.w3c.dom.HTMLButtonElement
+import kotlin.test.*
 
 class Main2Test {
 
     private val testElement: Element
         get() = checkNotNull(document.getElementById("test")) { "Test element not found" }
 
+    private var formatCalled = false
+    private val format: (String, Boolean) -> String = { _, _ ->
+        formatCalled = true
+        ""
+    }
+
     @Test
     fun addsFormatButton() {
         main2(testElement)
         val button = assertNotNull(testElement.querySelector("button"))
         assertEquals("Format", button.innerHTML)
+        assertIs<HTMLButtonElement>(button)
+    }
+
+    @Test
+    fun formatsOnButtonClick() {
+        main2(testElement, format)
+
+        val button = assertIs<HTMLButtonElement>(testElement.querySelector("button"))
+        button.click()
+
+        assertTrue(formatCalled)
     }
 
     @BeforeTest
