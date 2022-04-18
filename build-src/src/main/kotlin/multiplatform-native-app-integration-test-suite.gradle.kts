98bcde13d3612cc7b72a5a58504d305457d66bff
Gabriel Feo <gabriel@gabrielfeo.com>
2022-04-19 00:43:27 +0100

    WIP: Move to convention plugin

---
+++ build-src/src/main/kotlin/multiplatform-native-app-integration-test-suite.gradle.kts
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 Gabriel Feo
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
+import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeHostTest
+
+plugins {
+    id("multiplatform-native-app")
+}
+
+kotlin {
+    val commonSourceSet = sourceSets.create("commonIntegrationTest")
+    targets.filterIsInstance<KotlinNativeTarget>().forEach {
+        val compilation = it.createIntegrationTestCompilation(commonSourceSet)
+        it.configureIntegrationTestBinaryAndTask(project, compilation)
+    }
+}
+
+fun KotlinNativeTarget.createIntegrationTestCompilation(
+    commonSourceSet: KotlinSourceSet,
+): KotlinNativeCompilation {
+    val mainCompilation = compilations.getByName("main")
+    return compilations.create("integrationTest") {
+        associateWith(mainCompilation)
+        defaultSourceSet {
+            dependsOn(commonSourceSet)
+            addImplementationDependencyOn(mainCompilation)
+        }
+    }
+}
+
+fun KotlinSourceSet.addImplementationDependencyOn(
+    compilation: KotlinNativeCompilation,
+) {
+    dependencies {
+        implementation(compilation.compileDependencyFiles + compilation.output.classesDirs)
+    }
+}
+
+fun KotlinNativeTarget.configureIntegrationTestBinaryAndTask(
+    project: Project,
+    compilation: KotlinNativeCompilation,
+) {
+    binaries {
+        test("integration", listOf(NativeBuildType.DEBUG)) {
+            this.compilation = compilation
+            project.registerIntegrationTestTask(compilation.target, testExecutable = this)
+        }
+    }
+}
+
+fun Project.registerIntegrationTestTask(
+    target: KotlinNativeTarget,
+    testExecutable: TestExecutable,
+) {
+    tasks.register("${target.name}IntegrationTest", KotlinNativeHostTest::class) {
+        targetName = testExecutable.target.name
+        executable(testExecutable.linkTaskProvider.flatMap { it.outputFile })
+        inputs.file(testExecutable.linkTaskProvider.map { it.outputFile })
+        configureTestReports()
+    }
+}
+
+fun KotlinNativeHostTest.configureTestReports() {
+    val resultsDir = project.layout.buildDirectory.dir("${TestingBasePlugin.TEST_RESULTS_DIR_NAME}/$name/binary")
+    val reportsDir = project.layout.buildDirectory.dir("${TestingBasePlugin.TESTS_DIR_NAME}/reports/integrationTest")
+    binaryResultsDirectory.set(resultsDir)
+    reports {
+        junitXml.outputLocation.set(resultsDir)
+        html.outputLocation.set(reportsDir)
+    }
+}
--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,12 +1,3 @@
-import org.gradle.testing.base.plugins.TestingBasePlugin.TESTS_DIR_NAME
-import org.gradle.testing.base.plugins.TestingBasePlugin.TEST_RESULTS_DIR_NAME
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
-import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
-import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeHostTest
-
 /*
  * Copyright (c) 2022 Gabriel Feo
  *
@@ -18,62 +9,10 @@ import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeHostTest
 plugins {
     id("multiplatform-native-app")
     id("multiplatform-native-app-release")
+    id("multiplatform-native-app-integration-test-suite")
 }
 
 version = "0.0.1"
-
-// TODO In convention plugin
-configureIntegrationTestSuite()
-
-fun Project.configureIntegrationTestSuite() {
-    kotlin {
-        val commonSourceSet = sourceSets.create("commonIntegrationTest")
-        targets.filterIsInstance<KotlinNativeTarget>().forEach {
-            val compilation = it.createIntegrationTestCompilation(commonSourceSet)
-            it.configureIntegrationTestBinaryAndTask(project, compilation)
-        }
-    }
-}
-
-fun KotlinNativeTarget.configureIntegrationTestBinaryAndTask(project: Project, compilation: KotlinNativeCompilation) {
-    binaries {
-        test("integration", listOf(NativeBuildType.DEBUG)) {
-            this.compilation = compilation
-            project.registerIntegrationTestTask(compilation.target, testExecutable = this)
-        }
-    }
-}
-
-fun Project.registerIntegrationTestTask(target: KotlinNativeTarget, testExecutable: TestExecutable) {
-    tasks.register("${target.name}IntegrationTest", KotlinNativeHostTest::class) {
-        targetName = testExecutable.target.name
-        executable(testExecutable.linkTaskProvider.flatMap { it.outputFile })
-        inputs.file(testExecutable.linkTaskProvider.map { it.outputFile })
-        val resultsDir = project.layout.buildDirectory.dir("$TEST_RESULTS_DIR_NAME/$name/binary")
-        val reportsDir = project.layout.buildDirectory.dir("$TESTS_DIR_NAME/reports/integrationTest")
-        binaryResultsDirectory.set(resultsDir)
-        reports {
-            junitXml.outputLocation.set(resultsDir)
-            html.outputLocation.set(reportsDir)
-        }
-    }
-}
-
-fun KotlinNativeTarget.createIntegrationTestCompilation(commonSourceSet: KotlinSourceSet): KotlinNativeCompilation {
-    val mainCompilation = compilations.getByName("main")
-    return compilations.create("integrationTest") {
-        associateWith(mainCompilation)
-        defaultSourceSet {
-            dependsOn(commonSourceSet)
-            mainCompilation.let {
-                dependencies.add(
-                    implementationConfigurationName,
-                    it.compileDependencyFiles + it.output.classesDirs,
-                )
-            }
-        }
-    }
-}
 
 dependencies {
     commonMainImplementation(project(":formatter"))
@@ -83,19 +22,3 @@ dependencies {
     commonTestImplementation("com.squareup.okio:okio-fakefilesystem:3.0.0")
     add("commonIntegrationTestImplementation", kotlin("test"))
 }
-
-//afterEvaluate {
-//    kotlin {
-//        macosX64 {
-//            compilations.map {
-//                """
-//                ${it.name}:
-//                    compileOnlyConfigurationName=${it.compileOnlyConfigurationName}
-//                    runtimeOnlyConfigurationName=${it.runtimeOnlyConfigurationName}
-//                    compileDependencyConfigurationName=${it.compileDependencyConfigurationName}
-//                    runtimeDependencyConfigurationName=${it.runtimeDependencyConfigurationName}
-//                """.trimIndent()
-//            }.joinToString("\n").let(::println)
-//        }
-//    }
-//}
