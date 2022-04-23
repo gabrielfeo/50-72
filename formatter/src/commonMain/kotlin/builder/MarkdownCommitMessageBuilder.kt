5f658f1a4a2c6db16a44d6f6180cac0bdec729e1
Gabriel Feo <gabriel@gabrielfeo.com>
2022-04-23 23:17:43 +0100

    Extract Tokenizer class

--- formatter/src/commonMain/kotlin/builder/MarkdownCommitMessageBuilder.kt
+++ formatter/src/commonMain/kotlin/builder/MarkdownCommitMessageBuilder.kt
@@ -8,8 +8,9 @@
 
 package builder
 
-import builder.Matcher.Comment
-import builder.Matcher.MarkdownToken.*
+import builder.TokenType.Comment
+import builder.TokenType.MarkdownToken
+import builder.TokenType.MarkdownToken.*
 
 internal data class MarkdownCommitMessageBuilder(
     private val commentChar: Char,
@@ -25,16 +26,17 @@ internal data class MarkdownCommitMessageBuilder(
 
     @Suppress("RemoveRedundantQualifierName")
     override fun appendBody(body: String) {
-        val matchers = arrayOf(Comment(commentChar), *Matcher.MarkdownToken.values())
+        val tokenizer = Tokenizer(commentChar)
         var currentPosition = 0
         while (currentPosition < body.lastIndex) {
-            val (matcher, match) = matchBodyPosition(currentPosition, body, matchers)
-            when (matcher) {
+            val token = tokenizer.tokenizePosition(currentPosition, body)
+            when (token.type) {
                 BoldText,
                 ItalicizedText,
                 Strikethrough,
                 PlainText,
-                Link -> sequence.append(match)
+                Link,
+                -> sequence.append(token.text)
                 BulletListItem,
                 NumberedListItem,
                 Table,
@@ -42,43 +44,64 @@ internal data class MarkdownCommitMessageBuilder(
                 HtmlElement,
                 Quote,
                 Heading,
-                EndPunctuation -> sequence.appendRaw(match)
-                StandalonePunctuation -> sequence.append(match.trim())
+                EndPunctuation,
+                -> sequence.appendRaw(token.text)
+                StandalonePunctuation -> sequence.append(token.text.trim())
                 ParagraphBreak -> sequence.breakParagraph()
                 is Comment,
                 WordSpacing,
-                SingleLineBreak -> {
+                SingleLineBreak,
+                -> {
                 }
             }
-            currentPosition += match.length
+            currentPosition += token.text.length
         }
     }
+}
 
-    private fun matchBodyPosition(
+private data class Tokenizer(
+    private val commentChar: Char,
+) {
+
+    private val tokenTypes = arrayOf(
+        Comment(commentChar),
+        *MarkdownToken.values(),
+    )
+
+    fun tokenizePosition(
         currentPosition: Int,
         body: String,
-        matchers: Array<Matcher>,
-    ): Pair<Matcher, String> {
-        for (matcher in matchers) {
-            val match = matcher.matchAt(body, currentPosition) ?: continue
-            return matcher to match
+    ): Token {
+        for (type in tokenTypes) {
+            val match = type.matchAt(body, currentPosition) ?: continue
+            return Token(type, match)
         }
         val currentPositionSnippet = body.substring(currentPosition, currentPosition + 10)
         error("No match for position $currentPosition: $currentPositionSnippet...")
     }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun TokenType.matchAt(
+        string: String,
+        index: Int,
+    ): String? {
+        val match = Regex(pattern).matchAt(string, index)
+        return match?.value
+    }
+
+    data class Token(
+        val type: TokenType,
+        val text: String,
+    )
 }
 
-private sealed interface Matcher {
+private sealed interface TokenType {
 
     val pattern: String
 
-    @OptIn(ExperimentalStdlibApi::class)
-    fun matchAt(string: String, index: Int): String? =
-        Regex(pattern).matchAt(string, index)?.value
-
     data class Comment(
         val commentChar: Char,
-    ) : Matcher {
+    ) : TokenType {
         //language=RegExp
         override val pattern = "$commentChar.*"
     }
@@ -87,7 +110,7 @@ private sealed interface Matcher {
     enum class MarkdownToken(
         //language=RegExp
         override val pattern: String,
-    ) : Matcher {
+    ) : TokenType {
         ParagraphBreak("""\n\s*\n"""),
         Heading("""#+[^\n]+"""),
         BoldText("""\*\*[^*]+\*\*"""),
